f99245afd52465db5d1ea5c8ab2d31af3b541f1f
Hoàng Hữu Mạnh <na187729288@gmail.com>
2022-12-04 15:59:25 +0700

    script chat vs gg analytic

--- pages/index.js
+++ pages/index.js
@@ -40,30 +40,39 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Haiconmeo" />
         <meta property="og:image" content="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/313414115_190230920179002_4850919535704207251_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=txGOlDwoHdoAX_eAX9z&_nc_ht=scontent.fdad3-5.fna&oh=00_AfC1-L227G6UIx6U7edfekwQqHPWy-t49Yctnb9-__cbnw&oe=6373BEAB" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N9XN9ZR3GZ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N9XN9ZR3GZ');
+        </script>
+        <script src="https://messenger.svc.chative.io/static/v1.0/channels/s5872f568-fe3a-4896-8236-4a4a150699f8/messenger.js?mode=livechat" defer="defer"></script>
       </Head>
 
       <VStack minHeight="100vh" bgGradient={colorMode === 'light' ? "linear(to-r, #1F1C2C, #928DAB)" : "linear(to-r, #0F2027, #203A43, #24243e)"}>
         <Container maxW="960">
-          <Header/>
-            <Masonry
+          <Header />
+          <Masonry
             breakpointCols={breakpointColumnsObj}
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column"
-            >
-            <BioCard/>
-            <ContactCard/>
-            <MyMessageCard/>
-            <SkillsCard/>
-            <BlogBtnCard/>
-            <ProjectsBtnCard/>
-            <CustomComponentOne/>
-            <CustomComponentTwo/>
-            <CustomComponentThree/>
-            <CustomComponentFive/>
-            <DownloadCv/>
-            <CustomComponentFour/>
-            </Masonry>
-          <Footer/>
+          >
+            <BioCard />
+            <ContactCard />
+            <MyMessageCard />
+            <SkillsCard />
+            <BlogBtnCard />
+            <ProjectsBtnCard />
+            <CustomComponentOne />
+            <CustomComponentTwo />
+            <CustomComponentThree />
+            <CustomComponentFive />
+            <DownloadCv />
+            <CustomComponentFour />
+          </Masonry>
+          <Footer />
         </Container>
       </VStack>
 
