d1b0b7e6e7b0c0315e2272536a602dbd1163d6a4
Hoàng Hữu Mạnh <na187729288@gmail.com>
2022-12-04 16:18:32 +0700

    update document

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,9 @@ export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head >
+          <script src="https://messenger.svc.chative.io/static/v1.0/channels/s5872f568-fe3a-4896-8236-4a4a150699f8/messenger.js?mode=livechat" defer="defer"></script>
+        </Head>
         <body>
           {/* 👇 Here's the script */}
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
