ff11b7418745753c04cdfa9aa8c0fa919949b064
Hoàng Hữu Mạnh <na187729288@gmail.com>
2022-12-04 16:09:15 +0700

    remove gg

--- pages/index.js
+++ pages/index.js
@@ -40,16 +40,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Haiconmeo" />
         <meta property="og:image" content="https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/313414115_190230920179002_4850919535704207251_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=txGOlDwoHdoAX_eAX9z&_nc_ht=scontent.fdad3-5.fna&oh=00_AfC1-L227G6UIx6U7edfekwQqHPWy-t49Yctnb9-__cbnw&oe=6373BEAB" />
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-N9XN9ZR3GZ"/>
-        <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-N9XN9ZR3GZ');
-        `}
-      </Script>
 
         <Script src="https://messenger.svc.chative.io/static/v1.0/channels/s5872f568-fe3a-4896-8236-4a4a150699f8/messenger.js?mode=livechat" defer="defer"></Script>
       </Head>
