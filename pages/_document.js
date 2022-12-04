2aa5092700f9da551e7bbf729b1497d322ae683e
Hoàng Hữu Mạnh <na187729288@gmail.com>
2022-12-04 16:20:50 +0700

    update gg analityc

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,8 @@
 import { ColorModeScript } from "@chakra-ui/react"
 import NextDocument, { Html, Head, Main, NextScript } from "next/document"
 import theme from "../theme"
+import Script from 'next/script'
+
 
 export default class Document extends NextDocument {
   render() {
@@ -10,6 +12,11 @@ export default class Document extends NextDocument {
       <Html lang="en">
         <Head >
           <script src="https://messenger.svc.chative.io/static/v1.0/channels/s5872f568-fe3a-4896-8236-4a4a150699f8/messenger.js?mode=livechat" defer="defer"></script>
+          <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','G-N9XN9ZR3GZ');`}}></Script>
         </Head>
         <body>
           {/* 👇 Here's the script */}
